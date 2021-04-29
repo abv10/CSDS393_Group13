2f196084fc7078c1e8ceabbc9591ab9b14de1044
bentrab <btrabold615@gmail.com>
2021-04-29 10:14:59 -0400

    Users Schema

---
+++ FourYearClassPlanningTool/Models/Users/UsersContext.cs
@@ -0,0 +1,27 @@
+﻿using FourYearClassPlanningTool.Models.Users.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FourYearClassPlanningTool.Models.Users
+{
+    public class UsersContext : DbContext
+    {
+        public UsersContext(DbContextOptions<RequirementsContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Schedule> Schedules { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<UserSchedule> UserSchedules { get; set; }
+    }
+}
