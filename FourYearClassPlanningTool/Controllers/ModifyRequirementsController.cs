fd56b87505f83848e5fa5c9022aa64a8455b2543
Jake <jprusky99@gmail.com>
2021-05-05 13:59:32 -0400

    added modify requirements views and controllers

---
+++ FourYearClassPlanningTool/Controllers/ModifyRequirementsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using FourYearClassPlanningTool.Models.Requirements;
+using FourYearClassPlanningTool.Models.Requirements.Entities;
+
+
+
+namespace FourYearClassPlanningTool.Controllers
+{
+    public class ModifyRequirementsController : Controller
+    {
+
+        private readonly RequirementsContext _requirementsContext;
+        public ModifyRequirementsController(RequirementsContext requirements)
+        {
+            _requirementsContext = requirements;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_requirementsContext);
+        }
+
+        public IActionResult Edit(string DegreeID)
+        {
+            ViewData["courses"] = _requirementsContext.Find<Degree>(DegreeID).Courses;
+            return RedirectToAction("Modify", "Modify");
+        }
+    }
+}
