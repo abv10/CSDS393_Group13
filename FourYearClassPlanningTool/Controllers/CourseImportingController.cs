e5b135f7b431a86c1ede604298014f4dd7d29d0f
Maed223 <72527044+Maed223@users.noreply.github.com>
2021-05-04 21:37:57 -0400

    Added course importing controller as well as its template, added tab on homepage

---
+++ FourYearClassPlanningTool/Controllers/CourseImportingController.cs
@@ -0,0 +1,37 @@
+﻿using FourYearClassPlanningTool.Models.Users;
+using FourYearClassPlanningTool.Models.Users.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FourYearClassPlanningTool.Controllers
+{
+    public class CourseImportingController : Controller
+    {
+        private readonly UsersContext _contexts;
+
+        public CourseImportingController(UsersContext context)
+        {
+            _contexts = context;
+        }
+
+        // GET: Courses
+        public IActionResult Index()
+        {
+            return View(_contexts.Courses.ToList());
+        }
+
+
+        public void Add(string CourseId)
+        {
+            var course = _contexts.Courses.Find(CourseId);
+            var user = _contexts.Users.Find(User.Identity.Name);
+            user.CompletedCourses.Add(course);
+            _contexts.Update(user);
+            _contexts.SaveChanges();
+
+        }
+    }
+}
